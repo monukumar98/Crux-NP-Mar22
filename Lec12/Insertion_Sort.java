ffd46af8a56110fd46aa93dcf3bade3e845f8714
monukumar98 <monu012as@gmail.com>
2022-05-08 16:15:39 +0530

    Binary Search

---
+++ Lec12/Insertion_Sort.java
@@ -0,0 +1,31 @@
+package Lec12;
+
+public class Insertion_Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 2, 1, 3, 6, 7, 5 };
+		Sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+	public static void Sort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			int j = i - 1;
+			int item = arr[i];
+			while (j >= 0 && arr[j] > item) {
+				arr[j + 1] = arr[j];
+				j--;
+
+			}
+			j++;
+			arr[j] = item;
+
+		}
+
+	}
+
+}
